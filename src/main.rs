cf2f2a23bc2b1d8336a5157316de9c05f0f356d2
Chris Bowen Jr <laylong@gmx.com>
2023-04-19 15:06:27 -0400

    Break away from template
    First true commit

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ use serde::{Serialize, Deserialize};
 use yansi::{Paint,Color};
 //use serde_json::{Key, Value};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct JsonObject {
+#[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
+struct Product {
     name: String,
+    sku: String,
+    stock: u32,
+    sold: u32,
+    released: String,
+    retired: bool,
 }
 
 fn user_input() -> String {
@@ -23,18 +28,24 @@ fn load_json_to_string(file_path: String) -> String {
     fs::read_to_string(file_path).unwrap_or_default()
 }
 
+fn json_object(json_string: String) -> Vec<Product> {
+    serde_json::from_str(&load_json_to_string(json_string))
+    .unwrap_or_default()
+}
+
 fn main() {
     let _time_thread = thread::spawn(|| {
         for _i in 0..10 {
-            println!("{}", Paint::green("Color and time check"));
-            thread::sleep(Duration::from_secs(1));
+            //println!("{}", Paint::green("Color and time check"));
+            thread::sleep(Duration::from_secs(60));
         }
     });
 
     let mut choice = String::new();
+    let save_file = json_object("./src/Products.json".to_owned());
     while choice != "quit" {
         choice = user_input().trim().to_lowercase().replace("\n","");
-        println!(" > {}", load_json_to_string("./src/some.json".to_owned()));
+        println!(" >{:?}", save_file[0].name);
     }
 
     //time_thread.join().unwrap();
