063a505adcddcdc110049346946ea3caf6cd87d7
Chris Bowen Jr <laylong@gmx.com>
2023-04-21 10:53:21 -0400

    Regex and title print
    Feature:
     - Input validation checks command against regex and then
       against SKU list.
     - Title card with basic command hints printed and in color.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#![allow(unused_imports, dead_code)]
 use std::io::stdin;
 use std::fs;
 use std::thread;
 use std::time::Duration;
 use serde::{Serialize, Deserialize};
 use yansi::{Paint,Color};
-
+use regex::Regex;
 //use serde_json::{Key, Value};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 struct Product {
@@ -47,12 +50,20 @@ impl InventoryApp {
             product_list: json_object("./src/Products.json".to_owned()),
         }
     }
-    fn valid_action_code(code: String) -> bool {
-        //println!("validator got: [{}]", code.clone().replace("\n",""));
-        if code == "test" {
+    pub fn valid_action_code(&self, code: String) -> bool {
+        let re = Regex::new(r"^\[(.+)\]\*(-?\d+)$").unwrap();
+        if re.is_match(code.as_str()) {
             return true;
         }
         return false;
+    }
+    pub fn valid_sku(&self, sku_to_check: String) -> bool {
+        for each_product in self.product_list.iter() {
+            if each_product.sku == sku_to_check {
+                return true;
+            }
+        }
+        false
     }
 }
 
@@ -65,14 +76,18 @@ fn main() {
     });
 
     let product_scanner = InventoryApp::new();
+    println!("\n---{}--- {}{}", Paint::green("Inventory Server Product Scanner"), Paint::yellow("V."), Paint::yellow(VERSION));
+    println!("Scan '{}' to add a new product.", Paint::blue("Q+[SKU](Product Name)#"));
+    println!("Scan '{}' with +/- numbers for stock/sell respectively.", Paint::blue("[SKU]*#"));
 
     loop {
         let choice = user_input().trim().replace("\n","");
         if choice == "quit" {
             return ();
         }
-        else if InventoryApp::valid_action_code(choice) {
+        else if product_scanner.valid_action_code(choice) {
             println!("valid");
+            product_scanner.valid_sku("test".to_owned());
         }
         else {
             println!("invalid");
