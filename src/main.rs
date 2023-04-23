31491fb350b82e5eacd5a1b6d315a51439bef9e5
Chris Bowen Jr <laylong@gmx.com>
2023-04-22 22:25:44 -0400

    Not valid build
    Tried to implement stock/sell code, but this goes against
    Rust's logic and is not valid. ChatGPT gave a working example,
    but that requires the stock/sell logic in the main loop, or at
    least not to use a &self to access and mutate the AppState.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,54 @@ use regex::Regex;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const STOCKSELL_REGEX: &str = r"^\[(.+)\]\*(-?\d+)$";
 
+enum TransactionType {
+    Sell,
+    Stock,
+    Gift,
+}
+
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 struct Product {
     name: String,
     sku: String,
-    stock: u32,
-    sold: u32,
+    stock: i32,
+    sold: i32,
     released: String,
     retired: bool,
+}
+
+impl Product {
+    fn new() -> Self {
+        Self {
+            name: "Default product".to_string(),
+            sku: "NOSKU".to_string(),
+            stock: 0,
+            sold: 0,
+            released: "01/01/1900".to_string(),
+            retired: false,
+        }
+    }
+    pub fn sell_product(&mut self, negative: i32) {
+        //self.stock += negative;
+        //self.sold -= negative;
+        self.print_product(TransactionType::Sell);
+    }
+    pub fn stock_product(&mut self, positive: i32) {
+        //self.stock += positive;
+        self.print_product(TransactionType::Stock);
+    }
+    pub fn gift_product(&mut self) {
+        //self.stock -= 1;
+        self.print_product(TransactionType::Gift);
+    }
+
+    pub fn print_product(&self, trans_type: TransactionType) {
+        match trans_type {
+            TransactionType::Stock => println!("{} [In Stock: {} Sold: {}]", Paint::green(&self.name), Paint::green(&self.stock), self.sold),
+            TransactionType::Sell => println!("{} [In Stock: {} Sold: {}]", Paint::red(&self.name), self.stock, Paint::red(&self.sold)),
+            TransactionType::Gift => println!("{} [In Stock: {} Sold: {}]", self.name, self.stock, self.sold),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -65,14 +105,14 @@ impl InventoryApp {
         let sku = &action[0];
         let quantity: i32 = action[1].parse().unwrap();
 
-        if quantity > 0 {
-            println!("Stocking {} of {}", quantity, sku);
-        }
-        else if quantity < 0{
-            println!("Selling {} of {}", quantity, sku);
-        }
-        else {
-            println!("Giving away 1 of {}", sku);
+        for each_product in self.product_list.iter_mut() {
+            if *each_product.sku == sku.to_string() {
+                match quantity {
+                    x if x > 0 => each_product.stock_product(quantity),
+                    x if x < 0 => each_product.sell_product(quantity),
+                    _ => each_product.gift_product(),
+                };
+            }
         }
     }
     pub fn valid_action_code(&self, code: String) -> bool {
